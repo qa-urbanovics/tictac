cd7c3e6c4293e5f8ff2457177198e658cf87feca
aleksejs.urbanovics <aleksejs.urbanovics@bb-tech.eu>
2026-03-08 11:19:27 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? `/${repo}/` : '/',
+  base: '/tictac/',
 })
